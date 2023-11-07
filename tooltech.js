5121c97bfb753af199a388f7966e5824029e9f60
Logon Aluno <logonrmlocal@fiap.com.br>
2023-11-07 13:47:03 -0300

    AC

--- tooltech.js
+++ tooltech.js
@@ -2,6 +2,7 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const bodyParser = require("body-parser");
+const res = require("express/lib/response");
 
 // Configurando o roteamento para teste no Postman
 const app = express();
@@ -18,20 +19,31 @@ mongoose.connect('mongodb://127.0.0.1:27017/tooltech', {
 
 // Criando a model do usuário
 const usuarioSchema = new mongoose.Schema({
-  senha: { type: String },
-  email: { type: String, required: true }
+  email: { type: String, required: true },
+  senha: { type: String }
+  
 });
 
 const Usuario = mongoose.model("Usuario", usuarioSchema);
 
 // Configurando os roteamentos
 app.post("/cadastrousuario", async (req, res) => {
-  const senha = req.body.senha;
   const email = req.body.email;
+  const senha = req.body.senha;
+  
 
-  const novoUsuario = new Usuario({
-    senha: senha,
-    email: email,
+
+  //ver se todos os campos estão preenchidos e se não tiver vai dar esse erro
+  if(email == null || senha == null){
+    return res.status(400).json({error : "Preencha todos os campos"})
+}
+
+//teste mais importante da ac
+const emailExiste = await Usuario.findOne({email : email});
+  
+const novoUsuario = new Usuario({
+  email: email,  
+  senha: senha,
   });
 
   try {
@@ -41,6 +53,7 @@ app.post("/cadastrousuario", async (req, res) => {
     res.status(400).json({ error });
   }
 });
+
 
 // Criando a model do produto ferramenta
 const ProdutoferramentaSchema = new mongoose.Schema({
@@ -77,9 +90,16 @@ app.post("/cadastroprodutoferramenta", async (req, res) => {
   }
 });
 
-app.get("/", async (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
+
+
+//rota para get cadastro
+app.get("/cadastroprodutoferramenta", async(req, res)=>{
+  res.sendFile(__dirname +"/cadastroprodutoferramenta.html");
+})
+
+app.get("/cadastrousuario", async(req, res)=>{
+  res.sendFile(__dirname +"/cadastrousuario.html");
+})
 
 // Configurando a porta
 app.listen(port, () => {
